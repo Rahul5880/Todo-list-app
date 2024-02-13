95860b55d82213def2a8c48ea95756fd71724de1
Rahul5880 <115461322+Rahul5880@users.noreply.github.com>
2024-02-13 20:19:39 +0530

    Add files via upload

---
+++ TODOLIST-APP/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+let name = "Rahul";
+function App() {
+  return (
+    <>
+      <TodoWrapper></TodoWrapper>
+    </>
+  );
+}
+
+export default App;
